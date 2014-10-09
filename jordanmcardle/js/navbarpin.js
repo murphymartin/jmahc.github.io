0bd94279c6da1842d2e3a524dbf5464cdfd341cd
jmahc <jtmcardle@crimson.ua.edu>
2014-10-09 00:10:28 -0500

    remove the navbar pin - possible problem for modal

--- jordanmcardle/js/navbarpin.js
+++ jordanmcardle/js/navbarpin.js
@@ -1,21 +1,2 @@
-$(function(){
-  var
-    $win = $(window),
-    $filter = $('.navbar'),
-    $filterSpacer = $('<div />', {
-      "class": "filter-drop-spacer",
-      "height": $filter.outerHeight()
-    });
-  $win.scroll(function(){
-    if(!$filter.hasClass('fix') && $win.scrollTop() > $filter.offset().top){
-      $filter.before($filterSpacer);
-      $filter.addClass("fix");
-    } else if ($filter.hasClass('fix')  && $win.scrollTop() < $filterSpacer.offset().top){
-      $filter.removeClass("fix");
-      $filterSpacer.remove();
-    }
-  });
-});
 
-
-$("#myModal").modal({'toggle'});
+$("#myModal").modal({"backdrop":"static"});
