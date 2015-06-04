edc91c91dc65ac48726be6b628e0f76824a59671
Jordan McArdle <jordan.mcardle@2-com.net>
2015-06-04 15:30:19 -0500

    Slide content and jquery animations

--- scripts/script.js
+++ scripts/script.js
@@ -27,47 +27,92 @@ Jmac.home.init_variables = function () {
     var t = this;
 
     t.queryParams = Jmac.getQueryParams();
-    t.background = $('.navbar.navbar-default');
-    t.cont = $('.container');
-    t.fullPageContent = $('#fullpage');
-    t.icon = $('.intro div ul li i');
-    t.visualStudio = $('.devicons-visualstudio');
-    t.msqlServer = $('.devicons-msql_server');
-    t.toolTip = $('[data-toggle="tooltip"]');
+
+    //FullPageJS
+    t.$fullPageContent = $('#fullpage');
+
+    //Navbar items
+    t.$background = $('.navbar.navbar-default');
+    t.$btn = $('#hamburger');
+    t.$navbarContent = $('#navbar');
+    t.$navbarCollapse = $(".navbar-collapse");
+    t.$navbarLink = $(".navbar-nav li a");
+    t.$profile = $('.profile');
+    t.$contact = $('.profile-contact');
+
+    //Slide #3
+    t.$icon = $('.intro div ul li i');
+    t.$toolTip = $('[data-toggle="tooltip"]');
+    t.$visualStudio = $('.devicons-visualstudio');
+    t.$msqlServer = $('.devicons-msql_server');
+
+    //Slide #4
+    t.email = "jordanmcardle@gmail.com";
+    t.$emailMe = $('#form-submit');
+    t.$emailName = $('#form-name');
+    t.$emailComments = $('#form-comments');
 };
 
 Jmac.home.init_methods = function () {
     var t = this;
 
-    t.toolTip.tooltip()
+    //initiates Bootstrap Tooltips
+    t.$toolTip.tooltip()
 
-    t.visualStudio.on('mouseenter', function () {
-        t.visualStudio.css('color','rgb(104,33,122)');
+    //initiates FullPageJS for slides
+    t.$fullPageContent.fullpage({
+        sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
+        anchors: ['intro', 'tech', 'about', 'contact'],
+        scrollingSpeed: 800
     });
 
-    t.visualStudio.on('mouseleave', function () {
-        t.visualStudio.css('color','rgb(255,255,255)');
+    //Profile name click
+    t.$profile.on('click', function () {
+        if (t.$contact.css('position') == 'absolute') {
+            t.$profile.animate({
+                left: '-100%',
+            }, 500);
+            t.$contact.fadeIn('slow');
+        }
+    });
+    t.$contact.on('click', function () {
+        t.$contact.fadeOut('fast');
+        t.$profile.animate({
+            left: '95px',
+        }, 500);
     });
 
-    t.msqlServer.on('mouseenter', function () {
-        t.msqlServer.css('color', '#a51d29');
+    //highlights m$-vs & m$-sql icons
+    t.$visualStudio.on('mouseenter', function () {
+        t.visualStudio.css('color', 'rgb(104,33,122)');
     });
 
-    t.msqlServer.on('mouseleave', function () {
-        t.msqlServer.css('color', 'rgb(255,255,255)');
+    t.$visualStudio.on('mouseleave', function () {
+        t.visualStudio.css('color', 'rgb(255,255,255)');
     });
 
-    t.icon.on('mouseenter', function () {
+    t.$msqlServer.on('mouseenter', function () {
+        t.$msqlServer.css('color', '#a51d29');
+    });
+
+    t.$msqlServer.on('mouseleave', function () {
+        t.$msqlServer.css('color', 'rgb(255,255,255)');
+    });
+
+    //highlights dev-icons
+    t.$icon.on('mouseenter', function () {
         $(this).addClass('colored');
     });
 
-    t.icon.on('mouseleave', function () {
+    t.$icon.on('mouseleave', function () {
         $(this).removeClass('colored');
     });
 
-    t.fullPageContent.fullpage({
-        sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
-        anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
-        scrollingSpeed: 800
+    t.$emailMe.on('click', function (e) {
+        e.preventDefault();
+        var name = t.$emailName.val();
+        var com = t.$emailComments.val();
+        var subject = "Contact Form from " + name +" via jordanmcardle.me";
+        window.location = 'mailto:' + t.email + '?subject=' + subject + '&body=' + com;
     });
 };
