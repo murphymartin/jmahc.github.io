8f53b6dc32f8fce5c2a5b090f6fc3dd60d5a7aa5
Jordan McArdle <Jordan.McArdle@2-com.net>
2015-09-30 13:59:18 -0500

    Mobile header issue

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,6 @@
 var Jmac = {}; //SiteName can be an abbreviation (e.g. SN for SiteName)
 Jmac.development = false;
 Jmac.debug = false;
-
-Jmac.getQueryParams = function () {
-    var params = [];
-    var query = window.location.search;
-    query = query.slice(1, query.length);
-    var nv = query.split('&');
-    for (var i = 0; i < nv.length; i++) {
-        var q = nv[i].split('=');
-        params[q[0]] = q[1];
-    }
-    return params;
-};
-
 Jmac.home = {};
 
 Jmac.home.init = function () {
@@ -26,7 +13,6 @@ Jmac.home.init = function () {
 Jmac.home.init_variables = function () {
     var t = this;
 
-    t.queryParams = Jmac.getQueryParams();
     t.$nav = $('nav');
     t.$arrow = $('.arrow.bounce');
     t.$page = $('body');
@@ -41,33 +27,35 @@ Jmac.home.init_methods = function () {
     t.$nav.affix();
 
     //navbar affix closes arrow
-    t.$nav.on('affixed.bs.affix', function(){
+    t.$nav.on('affixed.bs.affix', function () {
         t.$arrow.fadeOut(200);
     });
-    t.$nav.on('affixed-top.bs.affix', function() {
+    t.$nav.on('affixed-top.bs.affix', function () {
         t.$arrow.fadeIn(200);
     });
 
-    $(function() {
+    $(function () {
       //close navbar menu on click mobile
       $('.navbar-collapse ul li a:not(.dropdown-toggle)').bind('click touchstart', function () {
-          $('.navbar-toggle:visible').click();
+        $(".navbar-nav li a").on('click', function(event) {
+          $(".navbar-collapse").collapse('hide');
+        });
       });
 
       //Remove arrow bounce
-      if (t.$nav.hasClass('affix'))
+      if(t.$nav.hasClass('affix'))
       {
           t.$arrow.fadeOut(200);
       }
       //enables scrollspy for navbar
-      t.$page.scrollspy({ target: '#navbar' });
+      t.$page.scrollspy({target: '#navbar'});
 
       //smooth scrolling
-      $('a[href*=#]:not([href=#])').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      $('a[href*=#]:not([href=#])').click(function () {
+        if(location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
           var target = $(this.hash);
           target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-          if (target.length) {
+          if(target.length) {
             $('html,body').animate({
               scrollTop: target.offset().top
             }, 1000);
@@ -82,7 +70,7 @@ Jmac.home.init_methods = function () {
       initBannerVideoSize('.video-container .filter');
       initBannerVideoSize('.video-container video');
 
-      $(window).on('resize', function() {
+      $(window).on('resize', function () {
           scaleVideoContainer();
           scaleBannerVideoSize('.video-container .poster img');
           scaleBannerVideoSize('.video-container .filter');
@@ -98,7 +86,7 @@ Jmac.home.init_methods = function () {
 
     }
 
-    function initBannerVideoSize(element){
+    function initBannerVideoSize(element) {
 
         $(element).each(function(){
             $(this).data('height', $(this).height());
@@ -109,13 +97,13 @@ Jmac.home.init_methods = function () {
 
     }
 
-    function scaleBannerVideoSize(element){
+    function scaleBannerVideoSize(element) {
 
         var windowWidth = $(window).width(),
         windowHeight = $(window).height() + 5,
         videoWidth,
         videoHeight;
-        var windowRatio = windowHeight/windowWidth;
+        var windowRatio = windowHeight / windowWidth;
         console.log(windowHeight);
 
         $(element).each(function(){
@@ -123,14 +111,14 @@ Jmac.home.init_methods = function () {
 
             $(this).width(windowWidth);
 
-            if(windowWidth < 1000){
+            if(windowWidth < 1000) {
                 videoHeight = windowHeight;
                 videoWidth = videoHeight / videoAspectRatio;
 
-                if($(this).hasClass('no_stretch')){
+                if($(this).hasClass('no_stretch')) {
                   var increase = 300;
                   var newW = videoWidth + increase;
-                  var newH = (newW * videoHeight)/videoWidth;
+                  var newH = (newW * videoHeight) / videoWidth;
                   videoWidth = newW;
                   videoHeight = newH;
 
@@ -139,7 +127,7 @@ Jmac.home.init_methods = function () {
                 }
                 $(this).width(videoWidth).height(videoHeight);
             } else if (windowWidth > 1000) {
-                if (windowRatio < 0.7 && windowRatio > 0.58) {
+                if(windowRatio < 0.7 && windowRatio > 0.58) {
                 videoHeight = windowHeight;
                 videoWidth = videoHeight / videoAspectRatio;
                 videoWidth = (videoHeight / videoAspectRatio) + 200;
@@ -152,24 +140,4 @@ Jmac.home.init_methods = function () {
             $('.homepage-hero-module .video-container video').addClass('fadeIn animated');
         });
     }
-};
-
-
-Jmac.header = {};
-
-Jmac.header.init = function () {
-    var t = this;
-
-    t.init_variables();
-    t.init_methods();
-};
-
-Jmac.header.init_variables = function () {
-    var t = this;
-
-    t.button = $('#navbar');
-};
-
-Jmac.header.init_methods = function () {
-    var t = this;
 };
